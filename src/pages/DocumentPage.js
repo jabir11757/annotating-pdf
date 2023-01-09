f0a80dd76a74e5f6556be6a132508dccfce8760d
mdrayhanhowlader <rhowlader5314@gmail.com>
2023-01-09 10:37:52 +0600

    updated

--- src/pages/DocumentPage.js
+++ src/pages/DocumentPage.js
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 const DocumentPage = () => {
-    return (
-        <div>
-            Document Page
-        </div>
-    );
+  return <div>Document Page name edited by savage team member</div>;
 };
 
 export default DocumentPage;
